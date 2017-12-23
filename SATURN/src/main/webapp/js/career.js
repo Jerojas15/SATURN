e77947be6eaf8b6d4fd73d1ad19a51a1fe4e42d7
Julio <j.rojas.c123@gmail.com>
2017-12-22 21:52:40 -0600

    poco de js para mostrar user y career

---
+++ SATURN/src/main/webapp/js/career.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+  
+$(document).ready(function(){
+    showCareers();
+    
+});
+
+function addCareers(career){
+    var table = document.getElementById("careerTable");
+    
+    var template = "<li class='list-item'><div class='text-block-3'>{{University}}"+
+   "</div><div class='text-block-4'>{{Career}}"+
+"</div><div class='text-block-5'>{{Plan}}"+
+"</div><a href='info-carrera.html' class='button-11 w-button'></a></li>";
+    table.append(Mustache.render(template,career));
+};
+
+function showCareers(){
+     
+    
+    $.ajax({
+        //url: 'http://localhost:8080/BasesProyecto/rest/proyects/list',
+        dataType: 'json',
+        type: 'GET',
+        success: function( Careers, textStatus, jQxhr ){
+                console.log('success',Careers);
+                $.each(Careers, function(i , Career){
+                    addCareer(Career);
+
+                });
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+};
+
