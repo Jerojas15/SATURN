2107406e1646a0b273c68cd5169f05a4ee4e545a
Jose Paulo Yock <jyock1997@gmail.com>
2017-12-22 21:48:20 -0600

    Se crean los servlets encargados de atajar las consultas para cosas que incluyan a carreras y asistentes administrativos

---
+++ SATURN/src/main/java/view/career/CareerServlet.java
@@ -0,0 +1,72 @@
+package view.career;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import model.Career;
+
+/**
+ * Servlet implementation class CareerServlet
+ */
+
+@Path("/careers")
+public class CareerServlet {
+	
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public static List<Career> getCareers() {
+		List<Career> l = new ArrayList<Career>();
+		return l;
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public static Career getCareer(@PathParam("id") String idStr) {
+		Career c =  new Career();
+		return c;
+	}
+	
+	@POST
+	@Path("/create")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createCareer(Career career) {
+		
+		String result = "Resultado.......";
+		
+		return Response.status(200).entity(result).build();
+	}
+	
+	@PUT
+	@Path("/update/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response deleteAssistant(@PathParam("id") String idStr) {
+		
+		String result = "Resultado.......";
+		
+		return Response.status(200).entity(result).build();
+	}
+
+	@DELETE
+    @Path("/delete/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response deleteCareer(@PathParam("id") String idStr) {
+		
+		String result = "Resultado.......";
+		
+		return Response.status(200).entity(result).build();
+	}
+}
