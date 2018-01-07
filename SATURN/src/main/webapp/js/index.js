0a7e8553016a60a8c79df6cf61b752f3eb5d8b2b
Jose Paulo Yock <jyock1997@gmail.com>
2018-01-07 10:47:06 -0600

    Se agregan apartados en la interfaz para poder editar los profesores

--- SATURN/src/main/webapp/js/index.js
+++ SATURN/src/main/webapp/js/index.js
@@ -33,11 +33,15 @@ var USER_LIST_TEMPLATE4 = "</div></div></div></li>";
 var USER_LIST_TEMPLATE1 = "<li class=\"s_listitem2\"><div class=\"w-row\"><div class=\"s_center w-col w-col-6\"><div class=\"s_text1\">";
 var USER_LIST_TEMPLATE2 = "</div></div><div class=\"s_center w-col w-col-6\"><div class=\"s_flex1\"><div class=\"s_text1\">";
 var USER_LIST_TEMPLATE3 = "</div><div id=\"Btn_Edit_Delete\" class=\"s_flex2\" style=\"display: none;\"><a id=\"Btn_Edit_User\" value=\"";
-var USER_LIST_TEMPLATE4 = "\" href=\"#\" class=\"s_button6 w-button\"></a><a id=\"Btn_Delete_User\" value=\"";
-var USER_LIST_TEMPLATE5 = "\" href=\"#\" class=\"s_button7 w-button\"></a></div></div></div></div></li>";
+var USER_LIST_TEMPLATE4 = "\" type=\"";
+var USER_LIST_TEMPLATE5 = "\" href=\"#\" class=\"s_button6 w-button\"></a><a id=\"Btn_Delete_User\" value=\"";
+var USER_LIST_TEMPLATE6 = "\" type=\"";
+var USER_LIST_TEMPLATE7 = "\" href=\"#\" class=\"s_button7 w-button\"></a></div></div></div></div></li>";
 
 var usrType;
 var lastEditDeleteBtn;
+var editDeleteUserId;
+var editDeleteUserType;
 
 /*
  * Función la cual debe volver al inicio de la pagina;
@@ -81,6 +85,42 @@ function fShowAddTeacher() {
 	$("#AddUser").show();
 	$("#Btn_AddTeacherSubmit").show();
 	$("#Btn_AddTeacherCancel").show();
+}
+
+function fShowEditUser() {
+	var url;
+
+	editDeleteUserId = $(this).attr("value");
+	editDeleteUserType = $(this).attr("type");
+	$("#AddUser").show();
+	$("#Btn_UpdateUserSubmit").show();
+	$("#Btn_UpdateUserCancel").show();
+	switch (Number(editDeleteUserType)) {
+		case USR_TYPE_ASSISTANT:
+			url = URL_ASSISTANTS;
+			$("#Assistants").hide()
+			break;
+		case USR_TYPE_COORDINATOR:
+			//url = ;
+			break;
+		case USR_TYPE_TEACHER:
+			url = URL_TEACHERS;
+			$("#Teachers").hide()
+			break;
+	}
+	$.ajax({
+		method: 'GET',
+		url: url + "/" + editDeleteUserId,
+
+		success: function(result){
+			$("#TextBox_AddUser_UserName").attr("placeholder", result.userName);
+			$("#TextBox_AddUser_Name").attr("placeholder", result.name);
+			$("#TextBox_AddUser_LastName").attr("placeholder", result.lastName);
+		},
+		error: function(request, status, error){
+			alert("Ha ocurrido un error inesperado, porfavor recargue la página e intente de nuevo");
+		}
+	});
 }
 
 /*
@@ -138,6 +178,13 @@ function fLogIn() {
 	}
 }
 
+function fClearUserForm() {
+	$("#TextBox_AddUser_UserName").val("");
+	$("#TextBox_AddUser_Name").val("");
+	$("#TextBox_AddUser_LastName").val("");
+	$("#TextBox_AddUser_Password").val("");
+	$("#TextBox_AddUser_Confirm_Password").val("");
+}
 function fAddCareer() {
 	var university = $("#TextBox_AddCareer_University").val();
 	var career = $("#TextBox_AddCareer_CareerName").val();
@@ -213,9 +260,7 @@ function fAddAssistant() {
 }
 
 function fClearAddAssistant() {
-	$("#TextBox_AddUser_UserName").val("");
-	$("#TextBox_AddUser_Name").val("");
-	$("#TextBox_AddUser_LastName").val("");
+	fClearUserForm();
 	$("#Form_CareerSelector").hide();
 	$("#Btn_AddAssistantSubmit").hide();
 	$("#Btn_AddAssistantCancel").hide();
@@ -262,11 +307,7 @@ function fAddTeacher() {
 }
 
 function fClearAddTeacher() {
-	$("#TextBox_AddUser_UserName").val("");
-	$("#TextBox_AddUser_Name").val("");
-	$("#TextBox_AddUser_LastName").val("");
-	$("#TextBox_AddUser_Password").val("");
-	$("#TextBox_AddUser_Confirm_Password").val("");
+	fClearUserForm();
 	$("#Form_CareerSelector").hide();
 	$("#Btn_AddAssistantSubmit").hide();
 	$("#Btn_AddAssistantCancel").hide();
@@ -318,10 +359,13 @@ function fDisplayAssistants() {
 		success: function(result){
 			//alert(JSON.stringify(result));
 			for (i in result) {
-				$("#Assistants ul").append(	USER_LIST_TEMPLATE1 + result[i].id +
-					 						USER_LIST_TEMPLATE2 + result[i].userName +
-											USER_LIST_TEMPLATE3 + result[i].name + " " + result[i].lastName +
-											USER_LIST_TEMPLATE4);
+				$("#Assistants ul").append(	USER_LIST_TEMPLATE1 + result[i].userName +
+					 						USER_LIST_TEMPLATE2 + result[i].name + " " + result[i].lastName +
+											USER_LIST_TEMPLATE3 + result[i].id +
+											USER_LIST_TEMPLATE4 + USR_TYPE_ASSISTANT +
+											USER_LIST_TEMPLATE5 + result[i].id +
+											USER_LIST_TEMPLATE6 + USR_TYPE_ASSISTANT +
+											USER_LIST_TEMPLATE7);
 			}
 		},
 		error: function(request, status, error){
@@ -348,14 +392,174 @@ function fDisplayTeachers() {
 				$("#Teachers ul").append(	USER_LIST_TEMPLATE1 + result[i].userName +
 					 						USER_LIST_TEMPLATE2 + result[i].name + " " + result[i].lastName +
 											USER_LIST_TEMPLATE3 + result[i].id +
-											USER_LIST_TEMPLATE4 + result[i].id +
-											USER_LIST_TEMPLATE5);
+											USER_LIST_TEMPLATE4 + USR_TYPE_TEACHER +
+											USER_LIST_TEMPLATE5 + result[i].id +
+											USER_LIST_TEMPLATE6 + USR_TYPE_TEACHER +
+											USER_LIST_TEMPLATE7);
 			}
 		},
 		error: function(request, status, error){
 			console.log("[Login] Error: " + error);
 		}
 	});
+}
+
+function fEditUser() {
+	var url;
+	var jObj;
+	var userName = $("#TextBox_AddUser_UserName").val();
+	var password = $("#TextBox_AddUser_Password").val();
+	var confirmPassword = $("#TextBox_AddUser_Confirm_Password").val();
+	var name = $("#TextBox_AddUser_Name").val();
+	var lastName = $("#TextBox_AddUser_LastName").val();
+
+	switch (Number(editDeleteUserType)) {
+		case USR_TYPE_ASSISTANT:
+			url = URL_ASSISTANTS;
+			break;
+		case USR_TYPE_COORDINATOR:
+
+			break;
+		case USR_TYPE_TEACHER:
+			url = URL_TEACHERS;
+			break;
+	}
+	jObj = {};
+	if (userName || (password && confirmPassword) || name || lastName) {
+		if (password === confirmPassword) {
+			if (userName)
+				jObj.userName = userName;
+			if (password)
+				jObj.password = password;
+			if (name)
+				jObj.name = name;
+			if (lastName)
+				jObj.lastName = lastName;
+			$.ajax({
+				method: 'PUT',
+				url: url + "/" + editDeleteUserId,
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				data: JSON.stringify(jObj),
+
+				success: function(result){
+					console.log("[Login] Result " + JSON.stringify(result));
+
+					if(result.status === "OK"){
+						fClearEditUser();
+					}
+				},
+
+				error: function(request, status, error){
+					console.log("[Login] Error: " + error);
+				}
+			});
+		} else {
+			alert("Contraseña no es igual")
+		}
+
+	} else {
+		alert("Se debe mostrar mensaje de que al menos un valor debe cambiar");
+	}
+}
+
+function fClearEditUser() {
+	$("#TextBox_AddUser_UserName").attr("placeholder", "Correo Institucional");
+	$("#TextBox_AddUser_UserName").val("");
+	$("#TextBox_AddUser_Password").val("");
+	$("#TextBox_AddUser_Confirm_Password").val("");
+	$("#TextBox_AddUser_Name").val("");
+	$("#TextBox_AddUser_LastName").val("");
+	$("#Btn_UpdateUserSubmit").hide();
+	$("#Btn_AddTeacherCancel").hide();
+	$("#AddUser").hide();
+
+	switch (Number(editDeleteUserType)) {
+		case USR_TYPE_ASSISTANT:
+			fDisplayAssistants();
+			break;
+		case USR_TYPE_COORDINATOR:
+
+			break;
+		case USR_TYPE_TEACHER:
+			fDisplayTeachers();
+			break;
+	}
+	editDeleteUserId = null;
+	editDeleteUserType = null;
+}
+
+function fConfirmDeleteUser() {
+	editDeleteUserId = $(this).attr("value");
+	editDeleteUserType = $(this).attr("type");
+	switch (Number(editDeleteUserType)) {
+		case USR_TYPE_ASSISTANT:
+			$("#Assistants").hide();
+			break;
+		case USR_TYPE_COORDINATOR:
+			//$("#Coordinators").hide();
+			break;
+		case USR_TYPE_TEACHER:
+			$("#Teachers").hide();
+			break;
+	}
+	$("#ConfirmDelete").show();
+}
+
+function fDeleteUser() {
+	var url;
+
+	$("#ConfirmDelete").hide();
+
+	switch (Number(editDeleteUserType)) {
+		case USR_TYPE_ASSISTANT:
+			url = URL_ASSISTANTS;
+			break;
+		case USR_TYPE_COORDINATOR:
+
+			break;
+		case USR_TYPE_TEACHER:
+			url = URL_TEACHERS;
+			break;
+	}
+	$.ajax({
+		method: 'DELETE',
+		url: url + "/" + editDeleteUserId,
+		success: function(result){
+			console.log("[Login] Result " + JSON.stringify(result));
+
+			if(result.status === "OK"){
+				fClearDeleteUser();
+			}
+		},
+
+		error: function(request, status, error){
+			console.log("[Login] Error: " + error);
+		}
+	});
+}
+
+function fClearDeleteUser() {
+	switch (Number(editDeleteUserType)) {
+		case USR_TYPE_ASSISTANT:
+			fDisplayAssistants();
+			break;
+		case USR_TYPE_COORDINATOR:
+			break;
+		case USR_TYPE_TEACHER:
+			fDisplayTeachers();
+			break;
+	}
+	editDeleteUserId = null;
+	editDeleteUserType = null;
+}
+
+function fEditCareer() {
+	alert("edit " + $(this).attr("value"));
+}
+
+function fDeleteCareer() {
+	alert("delete " + $(this).attr("value"));
 }
 
 $(document).ready(function(){
@@ -378,9 +582,14 @@ $(document).ready(function(){
 	$("#Btn_AddTeacher").click(fShowAddTeacher);
 	$("#Btn_AddTeacherSubmit").click(fAddTeacher);
 	$("#Btn_AddTeacherCancel").click(fClearAddTeacher);
+
+	$("#Btn_UpdateUserSubmit").click(fEditUser);
+	$("#Btn_UpdateUserCancel").click(fClearEditUser);
+	$("#Btn_DeleteUserSubmit").click(fDeleteUser);
+	$("#Btn_DeleteUserCancel").click(fClearDeleteUser);
 });
 /* Agregar un handler de tipo click a elementos de una lista que aun no han sido agregados */
-$(document).on('click','ul li',function(){
+$(document).on("click", "ul li",function(){
 	if (lastEditDeleteBtn) {
 		lastEditDeleteBtn.hide();
 	}
@@ -388,16 +597,13 @@ $(document).on('click','ul li',function(){
 	lastEditDeleteBtn.show();
 });
 
+$(document).on("click", "#Btn_Edit_User", fShowEditUser);
+$(document).on("click", "#Btn_Delete_User", fConfirmDeleteUser);
+$(document).on("click", "#Btn_Edit_Career", fEditCareer);
+//$(document).on("click", "#Btn_Delete_Career", fConfirmDeleteCareer);
+
 /* Saber si un boton fue oprimido y optener valores del boton
 $(document).on('click','#list #Btn1',function(){
     alert($(this).attr("value"));
 });
 */
-/* eliminar elementos de una lista eceptuando los elementos 0 y 1
-$("#myList li").each(function( index ) {
-	alert( index + ": " + $( this ).text() );
-	if(index !== 0 && index !==1){
-		$(this).remove();
-	}
-});
- */
